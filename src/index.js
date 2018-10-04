4cec18562b415db427ab8565d79312e37a9f0148
Antony <A.Krutenok@gmail.com>
2018-10-04 17:42:14 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 module.exports = function getZerosCount(number) {
-	// looking for 5 * 2 (one way to get 0)
- 	var zeros = 0;
-	while(number > 0)
-	{
-	  number = parseInt(number/5);
-	  zeros += number;
+	// looking for fives: 5 * 2 (one way to get 0)
+ 	let zeros = 0;
+  let num = number;
+	while(num > 0) {
+	  num = parseInt(num/5);
+	  zeros += num;
 	}
 
 	return zeros;
